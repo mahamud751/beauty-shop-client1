b56caa42e084c050ec9eaa9c6dcf58d370a36037
mahamud751 <55927923+mahamud751@users.noreply.github.com>
2021-12-19 20:50:18 +0600

    new3

--- src/components/Pages/Dashboard/DashboardMain/DashboardMain.js
+++ src/components/Pages/Dashboard/DashboardMain/DashboardMain.js
@@ -4,7 +4,7 @@ const DashboardMain = () => {
     return (
         <>
             <h2 sx={{ m: 5 }}>Welcome to our Shop</h2>
-            <img src="https://i.ibb.co/h8CJhFn/slideshow2-h2.png" alt="Second slide" />
+            <img className="img-fluid" src="https://i.ibb.co/h8CJhFn/slideshow2-h2.png" alt="Second slide" />
         </ >
     );
 };
--- src/components/Pages/Home/Home/HomeBanner/HomeBanner.js
+++ src/components/Pages/Home/Home/HomeBanner/HomeBanner.js
@@ -7,11 +7,8 @@ const HomeBanner = () => {
         <div className="homeBanner">
             <Carousel>
                 <Carousel.Item>
-                    <img
-
-                        src="https://i.ibb.co/h8CJhFn/slideshow2-h2.png"
+                    <img className="img-fluid" src="https://i.ibb.co/h8CJhFn/slideshow2-h2.png"
                         alt="Second slide"
-
                     />
                     <Carousel.Caption>
                         <h6>Look at the sunset, life is amazing, life is beautiful, life is what you make it. The key to success is to keep your head above </h6>
